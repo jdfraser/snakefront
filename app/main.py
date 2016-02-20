ed1d8383d3bb11f0f6c7ea25c6bdf2311575ce52
jdfraser <joshua.david.fraser@gmail.com>
2016-02-20 14:44:11 -0500

    Stubbed out basic framework

--- app/main.py
+++ app/main.py
@@ -3,3 +3,20 @@ import json
 import copy
 from pathfinding import shortest_path
 from heatmap import print_heatmap, gen_heatmap
+
+#retrieve and parse data from REST API
+state = get_state()
+heatmap = gen_heatmap(movedata)
+
+if state == 'food':
+	get_food()
+elif state == 'idle':
+
+elif state == 'aggressive':
+
+def get_state():
+	return 'food'
+	
+def get_food():
+	#determine target here
+	nextcoord, full_shortest_path = shortest_path(heatmap, headpos, target)
