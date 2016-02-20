6d439f9461945eb29da85274e5461770d0ad1c28
Ben Allison <ben@checkfront.com>
2016-02-20 12:53:09 -0800

    Updated old main.py to match new API

--- app/heatmap.py
+++ app/heatmap.py
@@ -24,13 +24,13 @@ def fractal_heat(data, coord, neck, depth, factor):
 			continue
 		fractal_heat(data, [x+coord[0], y+coord[1]], coord, depth-1, factor/3)
 
-def gen_heatmap(movedata, name='Snakefront-test', maxturns=7, userings=True):
-	state = copy.deepcopy(movedata)
-	width = len(state['board'])
-	height = len(state['board'][0])
+def gen_heatmap(requestdata, our_id='2ca1ab89-620c-4fbe-b876-179013470205', maxturns=7, userings=True):
+	state = copy.deepcopy(requestdata)
+	width = state['width']
+	height = state['height']
 	final = default_heatmap(width, height)
 	for snake in state['snakes']:
-		if snake['name'] == name:
+		if snake['id'] == our_id:
 			oursnake = snake
 			break
 	oursnakeHead = oursnake['coords'][0]
@@ -41,12 +41,12 @@ def gen_heatmap(movedata, name='Snakefront-test', maxturns=7, userings=True):
 		snakes = copy.deepcopy(state['snakes'])
 		for snake in snakes:
 			coords = snake['coords']
-			if snake['name'] != name and len(coords) >= oursnakeLength:
+			if snake['id'] != our_id and len(coords) >= oursnakeLength:
 				# parse heat around the head
 				fractal_heat(heatmap, coords[0], coords[1], turn, 33.0)
 			# todo: what if they ate food?
 			# Now parse the body
-			try: 
+			try:
 				for i in range(turn): coords.pop()
 			except IndexError: pass
 			for x,y in snake['coords']:
@@ -54,11 +54,9 @@ def gen_heatmap(movedata, name='Snakefront-test', maxturns=7, userings=True):
 		for x, col in enumerate(heatmap):
 			for y, heat in enumerate(col):
 				if heat != 1:
-					if not userings or (((x-oursnakeHead[0]) + (y-oursnakeHead[1])) == turn): 
+					if not userings or (((x-oursnakeHead[0]) + (y-oursnakeHead[1])) == turn):
 						final[x][y] += heat
 		final[oursnakeHead[0]][oursnakeHead[1]] = 99999
 		final[oursnakeNeck[0]][oursnakeNeck[1]] = 99999
 
 	return final
-
-
--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 import bottle
 import os
-#import json
-#import copy
-#from pathfinding import shortest_path
-#from heatmap import print_heatmap, gen_heatmap
+import json
+import copy
+from pathfinding import shortest_path
+from heatmap import print_heatmap, gen_heatmap
+
+name = 'Snakeoverflow'
+snake_id = '2ca1ab89-620c-4fbe-b876-179013470205'
 
 #retrieve and parse data from REST API
 @bottle.route('/static/<path:path>')
@@ -28,10 +31,11 @@ def index():
 def start():
 	data = bottle.request.json
 
-	# TODO: Do things with data
-
 	return {
-	'taunt': 'battlesnake-python!'
+		'name': name,
+		'color': '#1E90FF',
+		'head_url': 'https://github.com/Nebual/snakefront/blob/master/snake.png?raw=true',
+		'taunt': 'Online bookings for less!'
 	}
 
 
@@ -39,13 +43,80 @@ def start():
 def move():
 	data = bottle.request.json
 
-	# TODO: Do things with data
+	state = get_state()
+	heatmap = gen_heatmap(data, snake_id)
+	print_heatmap(heatmap)
+
+	oursnake = None
+	for snake in data['snakes']:
+		if snake['id'] == snake_id:
+			head = snake['coords'][0]
+			oursnake = snake
+
+	shortest = []
+	move = [0,0]
+	taunt = "Booking in progress"
+
+	if state == 'food':
+		for snack in data['food']:
+			nextcoord, full_shortest_path = ShortestPath(heatmap, head, snack)
+			print full_shortest_path, nextcoord
+			if shortest == [] or len(full_shortest_path) < len(shortest):
+				shortest = full_shortest_path
+				move = nextcoord
+		print "Recommend next move to " + str(move)
+	elif state == 'aggressive':
+	    pass
+	elif state == 'idle':
+	    pass
+
+	"""
+	#this was done at 6:20pm okay I was scared
+	for snake in data['snakes']:
+	    if snake['name'] != name and move in neighbours(snake['coords'][0]):
+	        taunt = "Get booked!"
+	        if len(snake['coords']) >= len(oursnake['coords']):
+	            #ahh they're bigger run away
+	            taunt = "RUN"
+	            if move[1] > head[1]:
+	                move[1] = head[1]
+	                move[0] = head[0] - 1
+	            elif move[1] < head[1]:
+	                move[1] = head[1]
+	                move[0] = head[0] + 1
+	            elif move[0] < head[0]:
+	                move[0] = head[0]
+	                move[1] = head[1] + 1
+	            elif move[0] > head[0]:
+	                move[0] = head[0]
+	                move[1] = head[1] - 1
+	        break
+	"""
+
+	if move in oursnake['coords']:
+	    pass #TODO: WTF DON'T MOVE INTO OURSELF!!
 
 	return {
-		'move': 'north',
+		'move': get_direction_from_target_headpos(head, move),
 		'taunt': 'battlesnake-python!'
 	}
 
+def get_state():
+	return 'food'
+
+def get_direction_from_target_headpos(head, move):
+	if move[1] > head[1]:
+	    nextmove = 'down'
+	elif move[1] < head[1]:
+	    nextmove = 'up'
+	elif move[0] > head[0]:
+	    nextmove = 'right'
+	elif move[0] < head[0]:
+	    nextmove = 'left'
+	else:
+	    print "UHHHH wat are you trying to move to yourself??"
+	    nextmove = 'left'
+	return nextmove
 
 @bottle.post('/end')
 def end():
@@ -62,19 +133,3 @@ def end():
 application = bottle.default_app()
 if __name__ == '__main__':
 	bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
-
-#state = get_state()
-#heatmap = gen_heatmap(movedata)
-
-#if state == 'food':
-#	get_food()
-#elif state == 'idle':
-#
-#elif state == 'aggressive':
-#
-#def get_state():
-#	return 'food'
-#	
-#def get_food():
-#	#determine target here
-#	nextcoord, full_shortest_path = shortest_path(heatmap, headpos, target)
