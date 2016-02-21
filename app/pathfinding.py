8013c1602900841dca72a34e83090f2f7b3a7a69
Vallery Lancey <vallerylancey@gmail.com>
2016-02-20 16:02:21 -0800

    Modularized pathfinding functions, changed function signatures

--- app/pathfinding.py
+++ app/pathfinding.py
@@ -3,12 +3,19 @@
 import networkx as nx
 
 
+#HOW TO USE:
+#	Graph = pathfinding.graphify(HeatMap)
+#Then run:
+#	pathfinding.cheapest_path(Graph, HeightOfPlayArea, HeadPos, TargetPos)
+#This returns a dict, see below for names/details
+
+
 
 #Heatmap is an array of the board, with higher values being less safe.
 #Headpos is the [column, row] of the snake head.
 
 #RETURNS a networkx graph
-def graphify(HeatMap, HeadPos, Target):
+def graphify(HeatMap):
 	Height = len(HeatMap[0])
 	Width = len(HeatMap)
 	G = nx.Graph()
@@ -36,11 +43,25 @@ def graphify(HeatMap, HeadPos, Target):
 			G.add_edge(cur, next)
 			G[cur][next]['weight'] = weight
 
-	Path = nx.shortest_path(G, source = HeadPos[0]*Height+HeadPos[1], target=Target[0]*Height+Target[1], weight='weight')
-	print Path
 	return G
-			
 
 
 
-print graphify([[0, 5], [1, 2], [3, 3]], [0,0], [2,1]).edges()
+
+def cheapest_path(G, Height, HeadPos, TargetPos):
+	Path = nx.shortest_path(G, source = HeadPos[0]*Height+HeadPos[1], target=TargetPos[0]*Height+TargetPos[1], weight='weight')
+	PosOfNextMove = [Path[1] // Height, Path[1] % Height]
+	weight = 0
+	for i in range(0, len(Path)-1):
+		weight += G[Path[i]][Path[i+1]]['weight'] #Get weight of move
+	return {
+		"path" : Path,
+		"length" : len(Path),
+		"nextPos" : PosOfNextMove,
+		"cost" : weight
+	}
+
+
+
+#G = graphify([[0, 5], [1, 2], [3, 3]], [0,0], [2,1])
+#print cheapest_path(G, [0,0], [2,1])
