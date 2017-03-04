85f2c013ed6c603aa70010e3f3f746e3201a722b
Unknown <robwillcode@gmail.com>
2017-03-04 15:56:28 -0800

    Add commandline parameters

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import json
 import copy
 import random
 import time
+import sys
 
 import pathfinding
 from heatmap import print_heatmap, gen_heatmap
@@ -12,6 +13,28 @@ import util
 name = 'camel_Snake'
 snake_id = ''
 
+starvation_Limit 				= 25
+starvation_Cost 				= 100
+hunger_Limit 					= 50
+hunger_Cost 					= 70
+preferred_Snek_Length_modifer 	= 2
+dominace_Length 				= 40
+follow_Cost_Limit 				= 200
+idle_Cost_Limit 				= 100
+server_Port						= '8080'
+name 							= 'camel_snake'
+
+if len(sys.argv) > 1:
+	starvation_Limit 				= int(sys.argv[1])   #25
+	starvation_Cost 				= int(sys.argv[2])  #100
+	hunger_Limit 					= int(sys.argv[3])   #50
+	hunger_Cost 					= int(sys.argv[4])   #70
+	preferred_Snek_Length_modifer 	= int(sys.argv[5])    #2
+	dominace_Length 				= int(sys.argv[6])   #40
+	follow_Cost_Limit 				= int(sys.argv[7])  #200
+	idle_Cost_Limit 				= int(sys.argv[8])  #100
+	server_Port						= sys.argv[9] 		#8080
+	name 							= sys.argv[10] #camel_snake
 
 # retrieve and parse data from REST API
 @bottle.route('/static/<path:path>')
@@ -102,25 +125,26 @@ def get_move(data, head, heatmap, graph):
 
 	move_name = ''
 
-	if (int(data['oursnake']['health_points']) < 25 and food_cost < 100):
+	if (int(data['oursnake']['health_points']) < starvation_Limit and food_cost < starvation_Cost):
 		move = food_move
-		move_name = 'food'
-	elif (int(data['oursnake']['health_points']) < 50 and food_cost < 70):
+		move_name = 'food-hungry'
+	elif (int(data['oursnake']['health_points']) < hunger_Limit and food_cost < hunger_Cost):
 		move = food_move
-		move_name = 'food'
-	elif ((longestSnakeLength + 2) >= len(data['oursnake']['coords']) and food_cost < 40):
+		move_name = 'food-easy-pickings'
+	elif ((longestSnakeLength + preferred_Snek_Length_modifer) >= len(data['oursnake']['coords']) and food_cost < dominace_Length):
 		move = food_move
-		move_name = 'food'
-	elif(follow_cost < 200):
+		move_name = 'food-dominate'
+	elif(follow_cost < follow_Cost_Limit):
 		move = follow_move
 		move_name = 'follow'
-	elif (idle_cost < 100):
+	elif (idle_cost < idle_Cost_Limit):
 		move = idle_move
 		move_name = 'idle'
 	else:
 		move = move_idle_dumb(data, head, heatmap, graph)
 		move_name = 'default'
 
+	print head, move, move_name
 	print "Recommend next move", move_name, get_direction_from_target_headpos(head, move), str(move)
 
 	return move
@@ -249,6 +273,6 @@ def end():
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 if __name__ == '__main__':
-	bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
+	bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', server_Port))
 
 
